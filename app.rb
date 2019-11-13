6861a30d43e2e41d483f90de3d29c3a1d2979d6c
sydnee510 <sbsydnee161416@gmail.com>
2019-11-12 20:33:08 -0800

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,18 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
-
-
+    get '/newteam' do 
+         #@newteam = ['Team Name', 'Coach', 'Point Guard', 'Shooting Guard', 'Power Forward', 'Small Forward', 'Center']
+        erb :newteam
+    end 
+    post "/team" do
+         @name = params[:name]
+         @coach = params[:coach]
+         @pg = params[:pg]
+         @sg = params[:sg]
+         @pf = params[:pf]
+         @sf = params[:sf]
+         @c = params[:c]
+         erb :team
+    end
 end
